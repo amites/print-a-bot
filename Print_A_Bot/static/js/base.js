499e56fb74a4d7fec447161f4fced9ca61e3bb75
Alvin Mites <alvin@mitesdesign.com>
2017-10-14 13:17:06 -0600

    djwifi running -- deploying to pi

--- Print_A_Bot/static/js/base.js
+++ Print_A_Bot/static/js/base.js
@@ -1,12 +1,23 @@
 var modal_id = '#ajax-modal';
 
+function notify_data(data, width, delay) {
+    if (data.msg.length > 0) {
+        $.bootstrapGrowl(data.msg, {
+            type: ((data.success) ? 'success' : 'error')
+            ,align: 'center'
+            ,width: typeof width !== 'undefined' ? width : 300
+            ,delay: typeof delay !== 'undefined' ? delay : 8000
+        });
+    }
+}
+
 function load_ajax_modal(url, action, template_id, callback, title) {
     var modal_obj = $(modal_id);
     var content_obj = $(modal_id+' .modal-content');
     var loading_ids = {
         'bg_id': 'loading-modal',
         'bar_id': 'loading-bar'
-    }
+    };
 
     var template = Handlebars.compile($('#template-loading-modal').html());
     $('body').append(template(loading_ids));
@@ -14,7 +25,7 @@ function load_ajax_modal(url, action, template_id, callback, title) {
     $.get(url, function(data) {
         $('#' + loading_ids.bg_id).remove();
         $('#' + loading_ids.bar_id).remove();
-        if (action == 'log') {
+        if (action === 'log') {
             var file_template = Handlebars.compile($('#template-log-display').html());
             var file_data = {
                 title: typeof title !== 'undefined' ? title : ''
@@ -42,31 +53,11 @@ function load_ajax_modal(url, action, template_id, callback, title) {
         $('#'+loading_ids['bar_id']).remove();
         notify_data({
             success: false
-            ,msg: '{%trans 'Failed to load page, please try again later.' %}'
-        })
+            ,msg: 'Failed to load page, please try again later.'
+        });
         return false;
     });
 }
-
-function bind_modal(selector) {
-    selector = typeof selector !== 'undefined' ? selector : '[data-toggle="modal"][data-href]';
-
-    var obj = $(this);
-    var selector_obj = $(selector);
-
-    selector_obj.on('click', function () {
-        var url = $(this).attr('data-href');
-        var action = $(this).attr('data-action') || false;
-        load_ajax_modal(url, action, null, null, obj.text())
-
-        var event = selector_obj.attr('data-event');
-        if (event) {
-            $(document).trigger(event);
-        }
-
-    });
-}
-
 
 function show_modal_content(content, title, footer) {
     var modal_obj = $(modal_id);
