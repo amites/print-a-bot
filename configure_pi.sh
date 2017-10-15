34c52db54263bce753721e02add818555e2fd9ae
Alvin Mites <alvin@mitesdesign.com>
2017-10-14 19:34:31 -0600

    updated config script

--- configure_pi.sh
+++ configure_pi.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+apt-get remove --purge hostapd -yqq
+apt-get update -yqq
+apt-get upgrade -yqq
+
+sudo apt-get install -y hostapd dnsmasq nginx
+sudo pip install gunicorn
+
 sudo cp system/nginx--print-a-bot /etc/nginx/sites-available/print-a-bot
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/print-a-bot /etc/nginx/sites-enabled/
@@ -9,4 +16,7 @@ sudo systemctl daemon-reload
 sudo systemctl start gunicorn_bot
 
 sudo service nginx restart
+
+
+sudo pip install -r Print_A_Bot/requirements-pi.txt
 
